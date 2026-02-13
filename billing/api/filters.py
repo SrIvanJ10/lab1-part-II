b135665dbad5fe99473c3e2298e0540482fe6946
Juanjo <juanjose.jjso88@gmail.com>
2026-02-13 11:28:54 +0100

    Add provider filter to Invoice list endpoint

--- billing/api/filters.py
+++ billing/api/filters.py
@@ -2,13 +2,16 @@ import django_filters
 from ..models import Invoice, Barrel
 
 
+        
 class InvoiceFilter(django_filters.FilterSet):
     invoice_no = django_filters.CharFilter(lookup_expr="icontains")
     issued_on = django_filters.DateFromToRangeFilter()
 
+    provider = django_filters.NumberFilter(field_name="lines__barrel__provider")
+
     class Meta:
         model = Invoice
-        fields = ["invoice_no", "issued_on"]
+        fields = ["invoice_no", "issued_on", "provider"]
         
 class BarrelFilter(django_filters.FilterSet):
     oil_type = django_filters.CharFilter(
